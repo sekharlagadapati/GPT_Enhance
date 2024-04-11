58299785376b6de73875b4fb187c5e1f080afc5e
sekharlagadapati <165019547+sekharlagadapati@users.noreply.github.com>
2024-04-11 19:54:12 +0530

    Update config.py

---
+++ src/config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    PINECONE_API_KEY = '*************'
+    OPENAI_API_KEY = '**************'
+    VECTOR_INDEX_NAME = 'gpt-test'
+    DEBUG = True
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///your_database.db'
